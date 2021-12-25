cd5dddd3ebf108a055796229ce95f5ecc7d53e3d
Dhans <sgdhans@gmail.com>
2021-12-25 20:55:38 +0530

    feat: preload functional instance
    working instance of preload.ts

--- src/app/containers/Overview.tsx
+++ src/app/containers/Overview.tsx
@@ -1,11 +1,24 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 import Layout from '../Components/Layout/Layout'
 
 export const Overview = () => {
+    let customMessage = "hello there main script"
+    const sendMessage = ()=>{
+        //@ts-expect-error
+        window.api.sendMsg(customMessage);
+        // customMessage= "";
+    }
     return (
         <div>
-            <Layout/>
-            
+        <Layout/>
+        <input type = "text" value={customMessage}/>
+            <Button variant="contained" color="primary" onClick={sendMessage}>
+               send Message
+             </Button>     
+             <Button variant="contained" color="primary" onClick={sendMessage}>
+               get memory usage
+             </Button>  
         </div>
     )
 }
--- src/server/preload.ts
+++ src/server/preload.ts
@@ -1,1 +1,9 @@
-console.log("harro from preload");
+const { contextBridge, ipcRenderer } = require("electron");
+console.log("preload loaded successfully 👏");
+
+const API = {
+  //@ts-expect-error
+  sendMsg: (mesg) => ipcRenderer.send("message", mesg),
+};
+
+contextBridge.exposeInMainWorld("api", API);
