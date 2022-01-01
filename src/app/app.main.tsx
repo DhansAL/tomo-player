47a5d1700d54154d3b16dfbfad4f41794e3a1992
Dhans <sgdhans@gmail.com>
2022-01-01 20:41:12 +0530

    fix: minor changes

--- src/app/app.main.tsx
+++ src/app/app.main.tsx
@@ -1,47 +1,21 @@
 import { Route, Routes } from "solid-app-router";
+import { Library } from "./Containers/Library";
+import { Overview } from "./Containers/Overview";
+import { Settings } from "./Containers/Settings";
 
-import { Link } from "solid-app-router";
 /**
+ im dumb
  * 
- * @bug - Solid router seems to be not working as it is supposed to be with electron
- * 
- * due to this reason the build will be shifted back to react+electron 
- * and wont return in any time soon. 
- * 
- * tbh i am disappointed :(
  */
 
 export const App = () => {
-  const Nav = () => {
-    return (
-      <>
-      <div style={{display:"flex"}}>
-      <Link style={{"margin-right":"10px"}} href="/">homepage</Link>
-      <Link style={{"margin-right":"10px"}} href="/library">new component</Link>
-      </div> 
-      </>
-    );
-  };
-  const Homepage = () => {
-    return (
-      <>
-        <div>homepage component.</div>
-      </>
-    );
-  };
-  const Component1 = () => {
-    return (
-      <>
-        <div>new component.</div>
-      </>
-    );
-  };
+ 
   return (
     <>
-    <Nav/>
       <Routes>
-        <Route  path="/" element={<Homepage />} />
-        <Route path="/library" element={<Component1 />} />
+        <Route  path="/" element={<Overview />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </>
   );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,10 @@
 import { render } from "solid-js/web";
-import { Router } from "solid-app-router";
+import { hashIntegration, Router } from "solid-app-router";
 import { App } from "./app.main";
 
 render(
   () => (
-    <Router>
+    <Router source={hashIntegration()}>
       <App />
     </Router>
   ),
