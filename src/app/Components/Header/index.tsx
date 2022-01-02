e28bf1a87f0f079ac9ef5e2ddbc7e47058747930
Dhans <sgdhans@gmail.com>
2022-01-02 10:55:00 +0530

    feat: header style

--- src/app/Components/Header/index.tsx
+++ src/app/Components/Header/index.tsx
@@ -1,13 +1,14 @@
-import { Alert } from "solid-bootstrap";
+import {  Nav, Navbar } from "solid-bootstrap";
 import { Component } from "solid-js";
 
 export const Header: Component = () => {
   //TODO: make an env file and put production|dev thingy and make a config folder in backend for that
   return (
     <>
-    <Alert variant="primary" dismissible transition>
-              This is a "primary" alert—check it out!{" "}
-            </Alert>
+    <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#">🍤 tomoplayerV2 | DEV</Navbar.Brand>
+                
+            </Navbar>
     </>
   );
 };
