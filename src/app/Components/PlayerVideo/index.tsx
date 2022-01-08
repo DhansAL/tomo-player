592f08317896cb5413b104b556e613fc4e776ef2
Dhans <sgdhans@gmail.com>
2022-01-08 22:58:28 +0530

    build: stick to native html player

--- src/app/Components/PlayerVideo/index.tsx
+++ src/app/Components/PlayerVideo/index.tsx
@@ -1,40 +1,27 @@
 import { createEffect, createSignal, onMount, useContext } from "solid-js";
-import "./player.css";
 import { FileFolderContext } from "../../Contexts/FileContext";
-
-import Plyr from "plyr";
 export const PlayerVideo = () => {
   // context api
   const globalFileProperties = useContext(FileFolderContext);
   let videoPath = globalFileProperties.propertiesForAll().path;
+
+  //ref
   let playerRef: HTMLVideoElement;
-  //TODO:enhance this cuz it causes unnecesary rerenders
-  // window.setInterval(function () {
-  //   console.log(playerRef.currentTime);
-  // }, 1000);
-  Plyr.setup("#player");
+
 
   return (
-    <>
-    {/* <video
-        ref={playerRef}
-        style={{ height: "330px", width: "400px" }}
-        id="player"
-        class="js-player"
-       
-      ></video>
-     */}
+    <>   
+
+      
+      classic html5 player
       <video
-        playsinline
-        controls
-        style={{ height: "330px", width: "400px", }}
         id="player"
         class="player"
-        ref={playerRef}
+        controls
+        
+        style={{ height: "330px", width: "400px", }}
         src="E:\\voracious animes\\kanojo okarishimasu\\rent9.mp4"
       ></video>
-
-      <button> +++++++++++++</button>
     </>
   );
 };
