50ff33f3bcfd2634959541ca41a258a4fc05e6c1
Dhans <sgdhans@gmail.com>
2022-01-10 16:10:13 +0530

    chore: logic for iteration -1

--- src/app/Components/Subtitles/index.tsx
+++ src/app/Components/Subtitles/index.tsx
@@ -1,43 +1,50 @@
 import { createEffect, createSignal, onMount } from "solid-js";
-import { subtitleGateaway } from "../../modules/subtitles/subtitleGateaway";
 
+import { subtitleGateaway } from "../../modules/subtitles/subtitleGateaway";
 type subtitleProps = {
   time: number;
   duration: number;
 };
 export const Subtitles = (props: subtitleProps) => {
-  // dont destructure props cuz it causes rerender  - solid issue
+
   const [sub, setSub] = createSignal("weiner");
   const [load, setLoad] = createSignal(false);
 
-  //Parsing
-  //getting the file
   let subfile = "E:\\voracious animes\\kanojo okarishimasu\\rent 2.ass"; //temp
   let subObj: any;
   //stick to async ipc
   onMount(async () => {
     subObj = await subtitleGateaway(subfile);
-    console.log(subObj)
+    // console.log(subObj)
     setLoad(true);
     // console.log(load());
   });
-  
-  createEffect(()=>{
-    console.log(props.time,"time");
-    
+
+  createEffect(() => {
+    props.time
+    if (subObj) {
+      let i = Math.floor(props.time)
+      let start = subObj[i].start * 1000
+      let end = subObj[i].end * 1000;
+
+      if ((start < props.time * 1000
+        &&
+        end > props.time * 1000)) {
+
+        console.log("search for me now", "END-", end, "<-->", "START-", start);
+
+        if (props.time * 1000 < end * 1000) {
+          //we dont want to inc i till it passes the end
+          i = i - 1;
+        }
+      } else {
+        console.log("no subs to search", props.time * 1000);
+      }
+    }
   })
 
-  // console.log(subObj[Math.floor(props.time)].end,"end time");
-  const handleSubLoad = () => {
-    let i = 0;
-    while (i < subObj.length) {
-      console.log(subObj[i].end, "end time");
-      i++;
-    }
-  };
   return (
     <>
-      <button onclick={handleSubLoad}>ready</button>
       <div>current time to be consumed by subtitles {props.time}</div>
       <div>subtitle - {sub()}</div>
       <div>duration - {props.duration}</div>
