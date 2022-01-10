9c7fdb4dab2dab8a2b2b50ccde63b8eab9b83835
Dhans <sgdhans@gmail.com>
2022-01-10 15:25:03 +0530

    fix: subtitle iteration issue

--- src/app/Components/Subtitles/index.tsx
+++ src/app/Components/Subtitles/index.tsx
@@ -8,21 +8,36 @@ type subtitleProps = {
 export const Subtitles = (props: subtitleProps) => {
   // dont destructure props cuz it causes rerender  - solid issue
   const [sub, setSub] = createSignal("weiner");
+  const [load, setLoad] = createSignal(false);
 
   //Parsing
   //getting the file
   let subfile = "E:\\voracious animes\\kanojo okarishimasu\\rent 2.ass"; //temp
-
+  let subObj: any;
   //stick to async ipc
-  createEffect(async()=>{
-
-    let so =  await subtitleGateaway(subfile);
-    console.log(so);
+  onMount(async () => {
+    subObj = await subtitleGateaway(subfile);
+    console.log(subObj)
+    setLoad(true);
+    // console.log(load());
+  });
+  
+  createEffect(()=>{
+    console.log(props.time,"time");
     
   })
-  //   console.log(SUBBLOB());
+
+  // console.log(subObj[Math.floor(props.time)].end,"end time");
+  const handleSubLoad = () => {
+    let i = 0;
+    while (i < subObj.length) {
+      console.log(subObj[i].end, "end time");
+      i++;
+    }
+  };
   return (
     <>
+      <button onclick={handleSubLoad}>ready</button>
       <div>current time to be consumed by subtitles {props.time}</div>
       <div>subtitle - {sub()}</div>
       <div>duration - {props.duration}</div>
