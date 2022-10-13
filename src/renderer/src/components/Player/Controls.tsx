e53cae852daa1d68b9c6a568a789224b40ab3f11
Dhans <sgdhans@gmail.com>
2022-10-14 04:46:21 +0530

    screenfull and overlay on player
    starting implementing custom player

---
+++ src/renderer/src/components/Player/Controls.tsx
@@ -0,0 +1,17 @@
+
+
+export const Controls = () => {
+    return (
+        <div class="absolute p-5 w-full h-screen flex gap-10 border flex-col   z-10">
+            <div class="flex flex-row basis-1/4 justify-between border">
+                <p class="text-info">top part</p>
+            </div>
+            <div class="flex flex-row basis-3/4 justify-between border">
+                <p class="text-info">middle part</p>
+            </div>
+            <div class="flex flex-row basis-1/4 justify-between border">
+                <p class="text-info">bottom part</p>
+            </div>
+        </div>
+    )
+}
--- src/renderer/src/pages/Player.tsx
+++ src/renderer/src/pages/Player.tsx
@@ -1,12 +1,28 @@
 // import { PlayerStore } from "@renderer/stores/PlayerStore"
 // import { unproxy } from "@renderer/utils/unProxy";
+import { Controls } from "@renderer/components/Player/Controls";
+import screenfull from "screenfull";
+
 
 import { AiFillAlert } from 'solid-icons/ai'
+import { createEffect, onMount } from "solid-js";
 /**
  * @description core player of the app. handles everything you need
  * 
  */
 export const Player_Core = () => {
+    let playerRef: HTMLVideoElement;
+    let playerContainerRef: HTMLDivElement | undefined = undefined;
+
+    const toggleFullscreen = async () => {
+
+        await screenfull.toggle(playerContainerRef)
+    }
+    onMount(() => {
+        toggleFullscreen()
+    })
+
+    //ref - works like this in solid
 
     // const [store, setStore] = PlayerStore;
     const handleAddItem = () => {
@@ -16,12 +32,14 @@ export const Player_Core = () => {
 
 
     return (
-        // <video controls src="asset://D:/minami/passds.mp4" class="  min-h-screen w-full px-0 py-2 bg-base-100 "></video>
         <>
+            <div class=" w-full h-screen flex justify-center items-center" ref={playerContainerRef}>
+                <video src="asset://D:/minami/passds.mp4" class="min-h-screen w-full p-0 bg-base-100 " />
+                <Controls />
+            </div>
 
-            <p class="m-5 text-3xl text-red-300">TESTING STORE</p>
-            <AiFillAlert size={94} />
-            <button onclick={handleAddItem} class="btn btn-info m-10">click me</button>
+
+
         </>
     )
 }
